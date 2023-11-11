6aac34a8d4ff673fb48a8f58f9931696d356bd78
Elniel <corgoo7z49h@gmail.com>
2023-11-11 18:12:43 +0200

    Change machine class name

--- Machine.h
+++ Machine.h
@@ -1,11 +1,18 @@
 #ifndef MACHINE_H
 #define MACHINE_H
+
 #include "memory.h"
 #include "operation.h"
 class machine
 {
 	memory memory_ram;
 	memory _reg('R');
+
+
+    public:
+    istream & operator >> (istream &in,  fileName &c);
+
+
 
 }
 #endif

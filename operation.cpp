ab605f5732e2584f15ba3ffa02a702c841aee9ba
Elniel <corgoo7z49h@gmail.com>
2023-11-12 14:40:13 +0200

    nothing new

--- operation.cpp
+++ operation.cpp
@@ -10,24 +10,29 @@ Operation::Operation()
 }
 
 
-void Operation::Register_load_memory(unsigned char R_Value, short int  ST_value, memory &ram,memory &reg) {
+void Operation::Register_load_memory(unsigned char R_Value, short int  ST_value, Memory &ram,memory &reg) {
     reg.write_memory(R_Value, ram.read_memory(ST_value)); 
 }
 
-void Operation::Register_load_value(unsigned char R_Value, short int  ST_value, memory &ram,memory &reg) {
-    reg.write_memory(R_Value, ram.read_memory(ST_value)); 
+void Operation::Register_load_value(unsigned char R_Value, short int  ST_value, Memory &ram,memory &reg) {
+    reg.write_memory(R_Value, ST_value); 
 }
 
-void Operation::move_to_another_Register(unsigned char R_Value, short int  ST_value, memory &ram) {
-
-    
-
+void Operation::move_to_another_Register(unsigned char R_Value, short int  ST_value, Memory &reg) {
+    reg.write_memory(ST_value, ram.read_memory(R_Value));
+    ram.delete_value(R_Value);
 }
+
+//bool write_memory(short int call_address, short int value);
+// short int read_memory(short int call_address);
+// void delete_value(short int address); // delete value in address 
+// bool write_memory(short int call_address, short int value);
+// int get_command(short int address);//the order
+// void write_command(short int address, char order, char R,short int ST);
 
 void Operation::add_two_s_complement(unsigned char R_Value, unsigned char S_Value ,unsigned char T_value, memory &ram) {
 
-    unsigned char num1 = ram.read_memory(R_Value);
-    unsigned char num2 = ram.read_memory(S_Value);
-    ram.write_memory(num1 + num2, ram.read_memory(T_value));
-
+    unsigned char num1 = reg.read_memory(S_Value);
+    unsigned char num2 = reg.read_memory(T_Value);
+    reg.write_memory(R_Value, num1 + num2);
 }
